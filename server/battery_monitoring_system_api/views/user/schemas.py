3a22d11dca888cb44af432526c061a69561bae5b
Mikuláš Heřman <mikulas.herman@aipberoun.cz>
2025-06-08 20:19:17 +0200

    feat - update schemas

--- server/battery_monitoring_system_api/views/user/schemas.py
+++ server/battery_monitoring_system_api/views/user/schemas.py
@@ -7,7 +7,7 @@ class UserQuerySchema(Schema):
 class UserDataSchema(Schema):
     userId: str = fields.Str(required=True, description="User ID")
     email: str = fields.Str(required=True, description="Email address")
-    sensors: List[str] = fields.List(fields.Str, required=True, description="List of sensor ids that user has access to")
+    sensors: List[str] = fields.List(fields.Str, description="List of sensor ids that user has access to")
 
 class UserUpdateSchema(Schema):
     userId: str = fields.Str(required=True, description="User ID")
